d220c2697af41db8f4a44fcbf2264bb2e41b94fb
yasha <yasha.morer@gmail.com>
2019-01-14 20:37:13 +0200

    setup project , first components assignment

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningComponent, ErrorComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-warning",
+  templateUrl: "./warning.component.html"
+})
+export class WarningComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
